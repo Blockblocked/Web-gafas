1a9c490b5d865a01436d8fb037df3713847bf938
Juanmanuelx24 <juanmanuelx242@gmail.com>
2024-05-09 12:31:43 -0500

    Base de datos
    BD mysql

--- Opticatec/Opticatec/settings.py
+++ Opticatec/Opticatec/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'principal'
 ]
 
 MIDDLEWARE = [
@@ -72,15 +73,16 @@ WSGI_APPLICATION = 'Opticatec.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'libreria4',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',  # El puerto predeterminado de MySQL en XAMPP es 3306
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
