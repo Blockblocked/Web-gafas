6cb948c827a172a8670b3f0a5dfae00feba5325b
Blockblocked <leduan12044@gmail.com>
2024-05-09 12:33:07 -0500

    Nombrando base de datos
    tiendagafas

--- Opticatec/Opticatec/settings.py
+++ Opticatec/Opticatec/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'Opticatec.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'libreria4',
+        'NAME': 'tiendagafas',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
